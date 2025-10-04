01359919a05f405051caefe81f8d83f04895283f
Christian <christianlouzada.castro@gmail.com>
2025-10-04 18:40:43 -0300

    x

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/MenteImpertubavel/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
